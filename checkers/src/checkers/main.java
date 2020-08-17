25cf15219340c1da23d145909aa5356904b272f4
ctorr <ctorr@LAPTOP-UGJ09KMA>
2020-08-17 02:44:03 -0400

    First limits on moves
    Made a method to calculate the possible moves of a specific piece as
    well as a list of moves that a piece can do to kill a unit. Tried to
    implement both into the program as well as implementing the rule that
    "if you can take a piece, you must". Currently quite buggy so i need to
    spend some time figuring out what went wrong/trying other ideas.

--- checkers/src/checkers/main.java
+++ checkers/src/checkers/main.java
@@ -14,39 +14,58 @@ public class main {
 			static ImageIcon black_king_image = new ImageIcon("C:/Users/ctorr/git/Checkers/checkers/src/resources/black_king.png");
 			static ImageIcon red_piece_image = new ImageIcon("C:/Users/ctorr/git/Checkers/checkers/src/resources/red_piece.png");
 			static ImageIcon red_king_image = new ImageIcon("C:/Users/ctorr/git/Checkers/checkers/src/resources/red_king.png");
+			
+	//setting up arrayLists for pieces that shall move, possible moves that a selected piece can make and
+	//which of those possible moves would kill a unit
+	//KillCombo is a list of Arrays of size 3 in the format: [target, unit to be killed, destination]
 			static ArrayList<Integer> movement_pieces = new ArrayList<Integer>();
 			static ArrayList<Integer> possibleMoves = new ArrayList<Integer>();
+			static ArrayList<Integer[]> KillMoves = new ArrayList<Integer[]>();
 			static boolean movementCheck = false;
+			
+			
+	//turn counter to keep in track the lengh of the game and also to use a tool for determining whos turn is it
+			static int turnCounter = 1;
+			
 	//------------------------------------main code---------------------------------------------------//
 	public static void main(String args[]) 
 	{
-		int turnCounter = 1;
 	JButton[] buttons = drawCheckerboard();
 	giveCommands(buttons);
 	
-	
 	}
 	//-------------------------------------------------------------------------------------------------//
-	//Moves piece from tile A to tile B
-	private static void Move(JButton[] buttons, int a, int b)
-	{
+	//Moves piece from tile A to tile B if it is a possible move
+	private static void Move(JButton[] buttons, int a, int b, String PassOrFail)
+	{	
 		movement_pieces.clear();
 		buttons[a].setActionCommand("unpressed");
-		buttons[a].setActionCommand("unpressed");
-		if (buttons[a].getIcon() != null && buttons[b].getIcon() == null)
+		buttons[b].setActionCommand("unpressed");
+		
+		if(PassOrFail == "Pass")
 		{
+			if (buttons[a].getIcon() != null && buttons[b].getIcon() == null)
+			{
 			buttons[b].setIcon(buttons[a].getIcon());
 			buttons[a].setIcon(null);
+			}
+			else  {System.out.println("Invalid Move.");}
+			UpgradeCheck(buttons, b);
+			System.out.println("Move done.");
 		}
-		else 
-		{System.out.println("Invalid Move");}
-		UpgradeCheck(buttons, b);
+		else if (PassOrFail == "PFail")
+		{System.out.println("Invalid Move.");}
+		else if (PassOrFail == "KFail")
+		{System.out.println("Invalid Move. You are able to eliminate an enemy unit with a diferent move.");}
+		
 		possibleMoves.clear();
-	}
+		KillMoves.clear();
+		
+		}
 	
 	//-------------------------------------------------------------------------------------------------//
 	//Kills a piece and verifies if its the final piece of that side//
-	private boolean DeathAndCheck(JButton[] buttons, final int j)
+	private static boolean DeathAndCheck(JButton[] buttons, final int j)
 	{	
 		if(buttons[j].getIcon() == black_piece_image || buttons[j].getIcon() == black_king_image)
 		{
@@ -67,26 +86,65 @@ public class main {
 		return true;
 	}
 	//-------------------------------------------------------------------------------------------------------//
-	//Calculates and returns the possible moves that one specific piece can make
-	private int[] PossibleMoves(JButton[] buttons, int i)
+	//Calculates the possible moves that one specific piece, which would kill a unit and puts those moves into 
+	//their respective ArrayLists
+	private static void CalculatePossibleMoves(JButton[] buttons, int i)
 	{
 		if(buttons[i].getIcon() == black_piece_image)
 		{
 			if (i % 8 == 0) {possibleMoves.add(i + 9);}
-			else if (i % 8 == 6) {possibleMoves.add(i + 7);}
+			else if (i % 8 == 7) {possibleMoves.add(i + 7);}
 			else {possibleMoves.add(i + 7);
 				  possibleMoves.add(i + 9);}
 		}
 		else if(buttons[i].getIcon() == red_piece_image) 
 		{
 			if (i % 8 == 0) {possibleMoves.add(i - 7);}
-			else if (i % 8 == 6) {possibleMoves.add(i - 9);}
-			else {possibleMoves.add(i + 7);
-				  possibleMoves.add(i + 9);}
+			else if (i % 8 == 7) {possibleMoves.add(i - 9);}
+			else {possibleMoves.add(i - 7);
+				  possibleMoves.add(i - 9);}
 		}
-		else if(buttons[i].getIcon() == black_king_image) {return null;}
-		else if(buttons[i].getIcon() == red_king_image) {return null;}
-		return null;
+		else if(buttons[i].getIcon() == black_king_image || buttons[i].getIcon() == red_king_image ) 
+		{
+			possibleMoves.add(i + 7);
+			possibleMoves.add(i + 9);
+			possibleMoves.add(i - 7);
+			possibleMoves.add(i - 9);
+		}
+		
+		
+		
+		for (Integer numb : possibleMoves) 
+		{
+			if(buttons[i].getIcon() == black_piece_image || buttons[i].getIcon() == black_king_image)
+			{
+				if(buttons[numb].getIcon() == black_piece_image || buttons[numb].getIcon() == black_king_image) 
+				{possibleMoves.remove(numb);}
+				else if(buttons[numb].getIcon() == red_piece_image || buttons[numb].getIcon() == red_king_image) 
+				{possibleMoves.remove(numb);
+				if(buttons[numb].getIcon() == null) {
+					Integer[] killMove = {i, numb, i + 2*(Math.abs(i - numb))};
+					KillMoves.add(killMove);
+				}
+				}
+				
+				
+			}
+			
+			else if(buttons[i].getIcon() == red_piece_image || buttons[i].getIcon() == red_king_image)
+			{
+				if(buttons[numb].getIcon() == red_piece_image || buttons[numb].getIcon() == red_king_image) 
+				{possibleMoves.remove(numb);}
+				else if(buttons[numb].getIcon() == black_piece_image || buttons[numb].getIcon() == black_king_image) 
+				{
+					possibleMoves.remove(numb);
+					if(buttons[numb].getIcon() == null) {
+						Integer[] killMove = {i, numb, i + 2*(Math.abs(i - numb))};
+						KillMoves.add(killMove);
+					}
+				 }
+			}
+		}
 	}
 	//-------------------------------------------------------------------------------------------------------//
 	//sets up Jframe with the pieces on each of the buttons
@@ -112,14 +170,13 @@ public class main {
 				if (buttons[i - 1].getBackground() == Color.red)
 					{buttons[i].setBackground(Color.black);}
 				else {buttons[i].setBackground(Color.red);}
-				System.out.println(i);
 				}
 				gridPanel.add(buttons[i]);
 			}
 			
 			for(int i = 0; i < 24; i++)
 			{
-				if (buttons[i].getBackground() == Color.red)
+				if (buttons[i].getBackground() == Color. black)
 				{
 					buttons[i].setIcon(black_piece_image);
 				}
@@ -155,7 +212,8 @@ public class main {
 		 System.out.println("The black side uprgraded one of their pieces to a king!");}
 		 }
 	//--------------------------------------------------------------------------------------------------------//
-	//set up interactions between buttons//
+	//set up interactions between buttons, mainly how by pressing one button after another, it tries to make a
+    //move in-game
 		public static void giveCommands(JButton[] buttons)
 		{
 			ActionListener test = new ActionListener() {
@@ -171,15 +229,47 @@ public class main {
 							{
 								buttons[i].setActionCommand("pressed");
 								movement_pieces.add(i);
+								
 								if(movement_pieces.size()==4) 
 								{
-								Move(buttons, movement_pieces.get(1), movement_pieces.get(2));
+								CalculatePossibleMoves(buttons, movement_pieces.get(1));
+								System.out.println(possibleMoves);
+								System.out.println(KillMoves);
+								if (KillMoves.isEmpty())
+								{
+									if(possibleMoves.contains(movement_pieces.get(2)))
+									{Move(buttons, movement_pieces.get(1), movement_pieces.get(2), "Pass");}
+									else
+									{Move(buttons, movement_pieces.get(1), movement_pieces.get(2), "PFail");}
+								}
+								
+								
+								else 
+								{
+									for (Integer[] KillCombo : KillMoves)
+									{
+										if(KillCombo[0] == movement_pieces.get(1)
+												&& KillCombo[2] == movement_pieces.get(2))
+										{
+											Move(buttons, movement_pieces.get(1), movement_pieces.get(2), "Pass");
+											DeathAndCheck(buttons, KillCombo[1]);
+											break;
+										}
+									}
+									if(movement_pieces.isEmpty() == false)
+									{
+										Move(buttons, movement_pieces.get(1), movement_pieces.get(2), "KFail");
+									}
+								}
 								buttons[i].setActionCommand("unpressed");
 								}
-								System.out.println(movement_pieces);
+								//System.out.println(movement_pieces);
 							}
 						}
 					}
+					
+					
+					
 					else if (state == "pressed")
 					{
 						for (int i = 0; i < 64; i++)
@@ -188,7 +278,6 @@ public class main {
 							{
 								buttons[i].setActionCommand("unpressed");
 								movement_pieces.clear();
-								System.out.println(movement_pieces);
 							}
 						}
 					}
